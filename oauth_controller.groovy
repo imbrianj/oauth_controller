f8fd4f7109fbebe5592f71a3083263b4a2ce8db7
imbrianj <bjohnsonwsu@gmail.com>
2014-05-06 21:04:20 -0700

    Initial commit

---
+++ oauth_controller.groovy
@@ -0,0 +1,155 @@
+/**
+ * App Endpoint API Access Example
+ *
+ * Author: SmartThings, brian@bevey.org
+ *
+ * Taken almost verbatim from https://gist.github.com/aurman/9813279
+ */
+
+definition(
+    name: "OAuth Endpoint",
+    namespace: "ImBrian",
+    author: "Brian J.",
+    description: "OAuth endpoint for Universal Controller",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience%402x.png",
+    oauth: true
+)
+
+preferences {
+  section("Allow Endpoint to Control These Things...") {
+    input "switches", "capability.switch", title: "Which Switches?", multiple: true, required: false
+    input "locks",    "capability.lock",   title: "Which Locks?",    multiple: true, required: false
+  }
+}
+
+mappings {
+  path("/switches") {
+    action: [
+      GET: "listSwitches"
+    ]
+  }
+
+  path("/switches/:id") {
+    action: [
+      GET: "showSwitch"
+    ]
+  }
+
+  path("/switches/:id/:command") {
+    action: [
+      GET: "updateSwitch"
+    ]
+  }
+
+  path("/locks") {
+    action: [
+      GET: "listLocks"
+    ]
+  }
+
+  path("/locks/:id") {
+    action: [
+      GET: "showLock"
+    ]
+  }
+
+  path("/locks/:id/:command") {
+    action: [
+      GET: "updateLock"
+    ]
+  }
+
+  path("/mode/:mode") {
+    action: [
+      GET: "updateMode"
+    ]
+  }
+}
+
+def installed() {}
+
+def updated() {}
+
+// Switches
+def listSwitches() {
+  switches.collect{device(it,"switch")}
+}
+
+def showSwitch() {
+  show(switches, "switch")
+}
+
+void updateSwitch() {
+  update(switches)
+}
+
+// Locks
+def listLocks() {
+  locks.collect{device(it,"lock")}
+}
+
+def showLock() {
+  show(locks, "lock")
+}
+
+void updateLock() {
+  update(locks)
+}
+
+// Modes
+void updateMode() {
+  log.debug "mode change request: params: ${params}"
+
+  def newMode = params.mode
+  setLocationMode(newMode)
+}
+
+def deviceHandler(evt) {}
+
+private void update(devices) {
+  log.debug "update, request: params: ${params}, devices: ${devices.id}"
+
+  def command = params.command
+
+  if (command) {
+    def device = devices.find { it.id == params.id }
+
+    if (!device) {
+      httpError(404, "Device not found")
+    }
+
+    else {
+      if(command == "toggle") {
+        if(device.latestValue('switch') == "on") {
+          device.off();
+        }
+
+        else {
+          device.on();
+        }
+      }
+
+      else {
+        device."$command"()
+      }
+    }
+  }
+}
+
+private show(devices, type) {
+  def device = devices.find { it.id == params.id }
+  if (!device) {
+    httpError(404, "Device not found")
+  }
+
+  else {
+    def attributeName = type == "motionSensor" ? "motion" : type
+    def s = device.currentState(attributeName)
+    [id: device.id, label: device.displayName, value: s?.value, unitTime: s?.date?.time, type: type]
+  }
+}
+
+private device(it, type) {
+  it ? [id: it.id, label: it.label, type: type] : null
+}
